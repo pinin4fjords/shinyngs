5a679b7e5416f08aa15c55e5712c6884c5709bb8
pinin4fjords <pininforthefjords@gmail.com>
2016-02-26 12:39:01 +0000

    Fully functioning interactive heatmaps

---
+++ R/accessory.R
@@ -0,0 +1,34 @@
+#' Make machine variable names pretty for display
+#' 
+#' Just split out words using '_' and capitalise first letter
+#'
+#' @param vn A string to be prettified
+#'
+#' @return output Prettified string
+#'
+#' @export
+#' 
+#' @examples
+#' vn <- 'ugly_name_of_thing'
+#' > prettyifyVariablename(vn)
+#' [1] 'Ugly name of thing'
+
+prettifyVariablename <- function(vn) {
+    gsub("_", " ", ucfirst(tolower(vn)))
+}
+
+#' Capitalise the first letter of a string
+#'
+#' @param string A string
+#'
+#' @return output String with capitalised first letter
+#'
+#' @export
+#' 
+#' @examples
+#' > ucfirst('Example')
+#' [1] 'Example'
+
+ucfirst <- function(string) {
+    paste0(toupper(substr(string, 1, 1)), substr(string, 2, nchar(string)))
+} 
