3a73ac7a557d4b33bba6cfcde79d2d592199c47c
Sergio Oller Moreno <soller@almirall.com>
2025-11-09 08:28:21 +0100

    enrichment results support in make_app_from_files

--- exec/make_app_from_files.R
+++ exec/make_app_from_files.R
@@ -133,6 +133,30 @@ option_list <- list(
     help = "Column in differential results files holding q values/ adjusted p values."
   ),
   make_option(
+    "--enrichment_gene_sets",
+    type = "character",
+    default = NULL,
+    help = "Comma-separated list of the GMT files used in the enrichment analyses."
+  ),
+  make_option(
+    "--enrichment_filename_template",
+    type = "character",
+    default = NULL,
+    help = "Template of filenames containing enrichment results. For instance '{contrast_name}_enrichment_for_{geneset_type}.tsv' or, if up and down regulated results are provided separately '{contrast_name}.{geneset_type}.gsea_report_for_{target|reference}.tsv'. '{contrast_name}', '{geneset_type}', and optionally '{target|reference}' are substituted dynamically for each contrast and geneset type. If not given, no enrichment results are included"
+  ),
+  make_option(
+    "--enrichment_skip_missing",
+    action = "store_true",
+    default = FALSE,
+    help = "Ignore if any gene set enrichment result for any contrast is missing"
+  ),
+  make_option(
+    "--enrichment_gene_type_id",
+    type = "character",
+    default = "gene_name",
+    help = "Gene identifier in the enrichnent gene sets. Use this to specify that the gmt files represent genes with the gene name or an entrez id"
+  ),
+  make_option(
     c("-o", "--output_directory"),
     type = "character",
     default = NULL,
@@ -272,9 +296,99 @@ contrast_stats[[opt$assay_entity_name]] <- lapply(contrast_stats_files, function
   )
 })
 
+# Enrichment results:
+# To show enrichment results we need:
+# - The contrasts file opt$contrast_file
+# - The gmt enrichment files used
+# - The actual enrichment results
+#
+
+if (!is.null(opt$enrichment_filename_template)) {
+  # Ensure we have gene sets and contrasts
+  if (is.null(opt$contrast_file)) {
+    stop("When --enrichment_filename_template is given, --contrast_file is required")
+  }
+  if (is.null(opt$enrichment_gene_sets)) {
+    stop("When --enrichment_filename_template is given, --enrichment_gene_sets is required")
+  }
+
+  contrasts_df = read_metadata(opt$contrast_file)
+  genesets_files <- stringsToNamedVector(
+    opt$enrichment_gene_sets,
+    simplify_files = TRUE,
+    prettify_names = TRUE
+  )
+
+  gene_set_analyses <- list(
+    lapply(setNames(nm=names(genesets_files)), function(geneset_type) {
+      lapply(setNames(nm=contrasts_df$id), function(contrast_name) {
+        ctrst_info <- as.list(contrasts_df[contrasts_df$id == contrast_name,])
+        # Two types of opt$enrichment_filename_template are supported:
+        # - The template points to a single file for each contrast and geneset_type
+        # - The template points to two files for each contrast and geneset_type. In
+        #   this case, one file is for the up-regulated results and another file is
+        #   for the down-regulated results.
+        #
+        # The template may look like:
+        # "gsea_for_{contrast_name}_using_{geneset_type}_for_{target|reference}.tsv"
+        # Which for a contrast "disease_vs_healthy" with target="disease" and reference="healthy"
+        # and geneset_type "geo_bp", would result in shinyngs expecting two files named:
+        # up: "gsea_for_disease_vs_healthy_using_geo_bp_for_disease.tsv"
+        # down: "gsea_for_disease_vs_healthy_using_geo_bp_for_healthy.tsv"
+        #
+        # If the template does not include {target|reference} we assume there is
+        # a single file per contrast and geneset type, for instance:
+        # "gsea_for_{contrast_name}_using_{geneset_type}_results.tsv"
+        # becomes:
+        # "gsea_for_disease_vs_healthy_using_geo_bp_results.tsv"
+        if (grepl("target\\|reference", opt$enrichment_filename_template)) {
+          up_file <- opt$enrichment_filename_template
+          up_file <- gsub("{contrast_name}", contrast_name, up_file, fixed=TRUE)
+          up_file <- gsub("{geneset_type}", geneset_type, up_file, fixed=TRUE)
+          up_file <- gsub("{target|reference}", ctrst_info$target, up_file, fixed=TRUE)
+          
+          down_file <- opt$enrichment_filename_template
+          down_file <- gsub("{contrast_name}", contrast_name, down_file, fixed=TRUE)
+          down_file <- gsub("{geneset_type}", geneset_type, down_file, fixed=TRUE)
+          down_file <- gsub("{target|reference}", ctrst_info$reference, down_file, fixed=TRUE)
+          if (file.exists(up_file) && file.exists(down_file)) {
+            list("up" = up_file, "down" = down_file)
+          } else {
+            if (opt$enrichment_skip_missing) {
+              NULL
+            } else {
+              stop(sprintf("both enrichment files should exist: %s and %s", up_file, down_file))
+            }
+          }
+          
+        } else {
+          enrichment_file <- gene_set_template
+          enrichment_file <- gsub("{contrast_name}", contrast_name, enrichment_file, fixed=TRUE)
+          enrichment_file <- gsub("{geneset_type}", geneset_type, enrichment_file, fixed=TRUE)
+          if (file.exists(enrichment_file)) {
+            enrichment_file
+          } else {
+            if (opt$enrichment_skip_missing) {
+              NULL
+            } else {
+              stop(sprintf("enrichment file: %s does not exist", enrichment_file))
+            }
+          }
+        }
+      })
+    })
+  )
+  names(gene_set_analyses) <- names(assay_files)[contrast_stats_assay]
+} else {
+  gene_set_analyses <- list()
+  genesets_files <- list()
+}
+
+
+
 ################################################
 ################################################
-## Build the app                               ##
+## Build the app                              ##
 ################################################
 ################################################
 
@@ -294,7 +408,8 @@ experiments[[opt$assay_entity_name]] <- list(
       file = x,
       measure = "counts"
     )
-  })
+  }),
+  "gene_set_analyses" = gene_set_analyses
 )
 
 shiny_config <- list(
@@ -323,6 +438,12 @@ if (!is.null(opt$description)) {
 } else if (!is.null(opt$report_markdown_file)) {
   shiny_config[['report']] <- opt$report_markdown_file
 }
+
+if (length(genesets_files) > 0) {
+  shiny_config[["gene_set_id_type"]] <- opt$enrichment_gene_type_id
+  shiny_config[["gene_sets"]] <- genesets_files
+}
+
 
 myesel <- eselistfromConfig(
   shiny_config,
