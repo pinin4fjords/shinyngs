9f8be3231779c1bd4efa4e682b36b32417b4d4fa
Delfina Terradas <155591053+delfiterradas@users.noreply.github.com>
2026-03-30 17:18:28 -0300

    Accept spaces in variables in metadata

--- R/accessory.R
+++ R/accessory.R
@@ -827,7 +827,7 @@ read_metadata <- function(filename, id_col = NULL, sep = NULL, stringsAsFactors 
       read.delim(
         filename,
         sep = sep,
-        check.names = FALSE,
+        check.names = TRUE,
         header = TRUE,
         stringsAsFactors = stringsAsFactors
       )
@@ -845,6 +845,8 @@ read_metadata <- function(filename, id_col = NULL, sep = NULL, stringsAsFactors 
           filename
         )
       )
+    } else {
+      id_col <- make.names(id_col)
     }
 
     metadata <- metadata[match(unique(metadata[[id_col]]), metadata[[id_col]]), ]
